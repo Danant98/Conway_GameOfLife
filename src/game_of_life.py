84fcc43488606874399453908b0cdb1d3239045f
Daniel E. Antonsen <danieleantonsen@gmail.com>
2023-11-23 13:03:54 +0100

    update: added files and class in game of life file

---
+++ src/game_of_life.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+__author__ = 'Daniel Elisabethsønn Antonsen'
+
+# Import libraries and 
+import pygame as pg
+import numpy as np
+
+# Game of Life class object
+class GL:
+
+    # Width and height of screen
+    WIDTH, HEIGHT = 400, 400
+    # Colors
+    GREY, WHITE, GOLDEN = (128, 128, 128), (255, 250, 240), (255, 185, 15)
+
+    def __init__(self):
+        pass
+
+    def event_handler(self, events:list):
+        pass
+
+    def create_grid(self):
+        pass
+
+    def run(self):
+        pass
+
+
---
+++ src/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+__author__ = 'Daniel Elisabethsønn Antonsen'
+
+# Import libraries and 
+from game_of_life import GL as gl
+
+
+if __name__ == '__main__':
+    gl.run()
+
+
